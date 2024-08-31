f809625bb2267f95705f1ddb5e442c215e1e1b7d
penggrin12 <miner.sidor@gmail.com>
2024-08-31 17:20:16 +0300

    oopssss

--- app/config.py
+++ app/config.py
@@ -3,11 +3,11 @@ from pydantic import SecretStr
 
 
 class Settings(BaseSettings):
-    host: str = "localhost"
-    port: int = 3306
-    user: SecretStr = SecretStr("user")
-    password: SecretStr = SecretStr("password")
-    db: str = "database"
+    mysql_host: str = "localhost"
+    mysql_port: int = 3306
+    mysql_user: SecretStr = SecretStr("user")
+    mysql_password: SecretStr = SecretStr("password")
+    mysql_db: str = "database"
 
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
 
