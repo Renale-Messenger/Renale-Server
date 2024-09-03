ae2ff230c2138e59512ad1063ee522069255aded
Acemany <116539353+acemany@users.noreply.github.com>
2024-09-03 03:24:41 +0300

    Json that Dict but not List

--- app/applib.py
+++ app/applib.py
@@ -1,12 +1,13 @@
-from typing import Any, Dict, List, Union, Callable
+from typing import Any, Dict, List, Union
 from random import randint
 
 
-__all__ = ["Json", "random_id"]
+__all__ = ["Json", "JsonD", "random_id"]
 
 
 def random_id() -> int:
     return randint(0, 999_999_999)
 
 
-Json = Union[Dict[str, Any], List[Any]]
+JsonD = Dict[str, Any]
+Json = Union[JsonD, List[Any]]
